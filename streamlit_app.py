8169925503e6dd7f2571fa761fe6893cf7db451b
Nguyen15041992 <nguyendanhnguyenty1992@gmail.com>
2026-03-30 13:42:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("ROBOT BACCARAT")
+st.write("Dán kết quả vào đây để Robot gợi ý:")
+
+# Ô nhập liệu
+chuoi = st.text_input("Nhập chuỗi P B:", "")
+
+if chuoi:
+    st.write("Robot đang tính...")
+    st.success("Gợi ý: Đánh BANKER")
+
